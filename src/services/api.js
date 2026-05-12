b94ef3d83fb676e430f8f5dd8567b1071f1bf831
Breison Mendez <56001833+BreisonMendez124@users.noreply.github.com>
2026-05-12 18:58:03 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://ecommerce-env.eba-waahu3mq.us-east-2.elasticbeanstalk.com/api';
+const API_URL = 'https://ecommerce-env.eba-waahu3mq.us-east-2.elasticbeanstalk.com/api';
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
